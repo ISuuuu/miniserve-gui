a692c29f8a2bd2a5510170f1b39984dfd59fd22b
kim <zjlkim@outlook.com>
2026-04-01 10:04:01 +0800

    fixubuntu 下花屏及依赖安装

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,9 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    // 仅在 Linux 下禁用 WebKit 合成器，解决 VMware 虚拟机渲染问题
+    #[cfg(target_os = "linux")]
+    std::env::set_var("WEBKIT_DISABLE_COMPOSITING_MODE", "1");
+
     miniserve_gui_lib::run();
 }
